4e38295c2c17553c3a535a16d177167ea15bc92f
Mansoor Bahramand <mansoor.bahramand@gMB96db97.local>
2018-09-12 19:38:03 -0700

    initial commit ]

---
+++ exercises/variables-data-types/src/app.js
@@ -0,0 +1,19 @@
+// *****************************************
+// Variables/Data Types
+function willReturnAString() {
+    // Create a variable below called "theString" that contains any string:
+
+    return theString;
+}
+
+function willReturnABoolean() {
+    // Create a variable below called "theBoolean" that contains a boolean value:
+
+    return theBoolean;
+}
+
+function willReturnANumber() {
+    // Create a variable below called "theNumber" that contains a number value:
+
+    return theNumber;
+}
